725266b32de8d9ef0b1f0d7e8871bf6a162ca4c8
ashutosh sahu <ppaskumar@gmail.com>
2024-09-09 23:06:05 +0530

    d

--- src/components/Quizes.jsx
+++ src/components/Quizes.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import classes from "./Quizes.module.css"
+import classes from "./Quizes.module.css";
 
 const Quizes = () => {
-  return <div className={classes.quiz}>
-    <img src="public\ScienceQuiz.png" alt="not allowed" />
-  </div>;
+  return (
+    <div className={classes.quiz}>
+      <img src="ScienceQuiz.png" alt="not allowed" />
+    </div>
+  );
 };
 
 export default Quizes;
