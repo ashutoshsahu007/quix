eefae4451c2f7bcde79b6907f7ebd4db706025fd
ashutosh sahu <ppaskumar@gmail.com>
2024-09-11 09:53:55 +0530

    done

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -120,7 +120,7 @@ const Body = () => {
             <h1>Play Quiz</h1>
           </div>
           <div>
-            <p>
+            <p className={classes.para}>
               Welcome to Quizard, where knowledge meets fun! Unleash your
               intellect with our captivating quizzes spanning various topics.
               Challenge yourself, compete with friends, and embark on a journey
